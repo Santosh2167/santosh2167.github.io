11d6b2532a663e028a6574295f91921e648e849a
Santosh <santosh.poudyal@gmail.com>
2018-09-24 00:52:17 +1000

    Fixed issue of career section covering nav wjile scrolling, made career section responsive

--- script/scripts.js
+++ script/scripts.js
@@ -22,6 +22,7 @@ var typed = new Typed('#typed', {
 $(".diamond-grid").diamonds({
     size : 200, // Size of diamonds in pixels. Both width and height. 
     gap : 5, // Pixels between each square.
+    loop: true,
     hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
     autoRedraw : true, // Auto redraw diamonds when it detects resizing.
     itemSelector : ".items" // the css selector to use to select diamonds-items.
